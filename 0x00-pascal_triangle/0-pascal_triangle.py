e2762001fbdb5325ca0395dfe95c917a96e7275a
franklinAnozie <cfanozie@gmail.com>
2024-06-03 13:14:26 +0100

    paschal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# An implementation of Paschal's Triangle in Python
+
+def pascal_triangle(n):
+    """ function definition of paschal triangle """
+    if n <= 0:
+        return []
+    retVal = [[1]]
+    for i in range(n - 1):
+        temp = [0] + retVal[-1] + [0]
+        row = []
+        for j in range(len(retVal[-1]) + 1):
+            row.append(temp[j] + temp[j + 1])
+        retVal.append(row)
+    return retVal
