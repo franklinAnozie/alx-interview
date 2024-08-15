5a94c88d815c4345b3620f5e7166747a5aa4c001
franklinAnozie <cfanozie@gmail.com>
2024-08-15 17:19:52 +0100

    removed unnecessary comments

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -5,23 +5,18 @@ from typing import List
 
 
 def rotate_matrix(matrix: List[List[int]]) -> List[List[int]]:
+    """ rotates a 2 by 2 matrix by 90 degrees """
     n = len(matrix)
 
     for layer in range(n // 2):
         first = layer
         last = n - 1 - layer
-        
+
         for i in range(first, last):
             offset = i - first
             top = matrix[first][i]
-        
+
             matrix[first][i] = matrix[last - offset][first]
             matrix[last - offset][first] = matrix[last][last - offset]
             matrix[last][last - offset] = matrix[i][last]
             matrix[i][last] = top
-
-
-if __name__ == "__main__":
-    to_rotate = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    matrix = rotate_matrix(to_rotate)
-    print(to_rotate)
