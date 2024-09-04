a6b4b62876db0c05f692b106f4d1be1716a5b31b
franklinAnozie <cfanozie@gmail.com>
2024-09-04 19:36:46 +0100

    attempted prime

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,45 +1,46 @@
 #!/usr/bin/python3
-""" prime numbers game """
 
-def makeSetInRound(num):
-    """ makes the sets """
-    return set(range(1, num + 1))
-
-
-def isPrime(num):
-    """ checks if a number is prime """
-    if num <= 1:
-        return False
-    if num <= 3:
-        return True
-    if num % 2 == 0 or num % 3 == 0:
-        return False
-    i = 5
-    while i * i <= num:
-        if num % i == 0 or num % (i + 2) == 0:
-            return False
-        i += 6
-    return True
-
+def sieve_of_eratosthenes(max_num):
+    """ Generate a list of primes up to max_num using the Sieve of Eratosthenes. """
+    is_prime = [True] * (max_num + 1)
+    is_prime[0] = is_prime[1] = False
+    p = 2
+    while p * p <= max_num:
+        if is_prime[p]:
+            for i in range(p * p, max_num + 1, p):
+                is_prime[i] = False
+        p += 1
+    return [p for p in range(max_num + 1) if is_prime[p]]
 
 def isWinner(x, nums):
-    """ assigns winner """
-    winner = {"Maria": 0, "Ben": 0}
-
-    for round in range(x):
-        primeCount = 0
-        s_o_n = makeSetInRound(nums[round])
-        for num in s_o_n:
-            if isPrime(num):
-                primeCount += 1
-        if primeCount % 2 or primeCount == 0:
-            winner["Ben"] += 1
-        else:
-            winner["Maria"] += 1
-
-    if winner["Ben"] > winner["Maria"]:
-        return "Ben"
-    elif winner["Maria"] > winner["Ben"]:
+    max_n = max(nums)
+    primes = sieve_of_eratosthenes(max_n)
+    
+    def play_game(n):
+        """ Simulate the game for a given n and return the winner. """
+        removed = [False] * (n + 1)
+        
+        turn = 0  # 0 for Maria, 1 for Ben
+        remaining_primes = primes
+        
+        for prime in remaining_primes:
+            if prime > n:
+                break
+            if not removed[prime]:
+                for multiple in range(prime, n + 1, prime):
+                    removed[multiple] = True
+                turn = 1 - turn
+        return "Ben" if turn == 0 else "Maria"
+    
+    winner_count = {"Maria": 0, "Ben": 0}
+    
+    for n in nums:
+        winner = play_game(n)
+        winner_count[winner] += 1
+    
+    if winner_count["Maria"] > winner_count["Ben"]:
         return "Maria"
+    elif winner_count["Ben"] > winner_count["Maria"]:
+        return "Ben"
     else:
         return None
